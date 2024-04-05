b2917344a5d189bc5deef724c9ccfa6a472a7c1e
Rahul <96195239+Mr-MRahul@users.noreply.github.com>
2024-04-05 22:02:02 +0530

    a

--- codes/data_scripts/generate_2D_val.m
+++ codes/data_scripts/generate_2D_val.m
@@ -1,5 +1,16 @@
 1;
-source('my_fspecial_gaussian.m');
+%source('my_fspecial_gaussian.m');
+1;
+function my_fspecial_gaussian(filter_xsize, sigxma)
+  filter_xsize = 21
+  % Create a Gaussian filter of size filter_size x filter_size with standard deviation sigma
+  x = -floor(filter_xsize / 2):floor(filter_xsize / 2);
+  y = x;
+  [X, Y] = meshgrid(x, y);
+  gauss_filter = exp(-(X.^2 + Y.^2) / (2*sigxma^2));
+  gauss_filter = gauss_filter ./ sum(gauss_filter(:));
+end
+
 function generate_mod_LR_bic()
 
 
