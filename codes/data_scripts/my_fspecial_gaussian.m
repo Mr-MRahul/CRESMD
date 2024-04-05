2c7d5c2cb55a3e269f4922dd771f4548687d0c9b
Rahul <96195239+Mr-MRahul@users.noreply.github.com>
2024-04-05 21:54:56 +0530

    a

--- codes/data_scripts/my_fspecial_gaussian.m
+++ codes/data_scripts/my_fspecial_gaussian.m
@@ -1,9 +1,9 @@
-function my_fspecial_gaussian(filter_size, sigma)
+function my_fspecial_gaussian(filter_xsize, sigxma)
   filter_size = 21
   % Create a Gaussian filter of size filter_size x filter_size with standard deviation sigma
-  x = -floor(filter_size / 2):floor(filter_size / 2);
+  x = -floor(filter_xsize / 2):floor(filter_xsize / 2);
   y = x;
   [X, Y] = meshgrid(x, y);
-  gauss_filter = exp(-(X.^2 + Y.^2) / (2*sigma^2));
+  gauss_filter = exp(-(X.^2 + Y.^2) / (2*sigxma^2));
   gauss_filter = gauss_filter ./ sum(gauss_filter(:));
 end
